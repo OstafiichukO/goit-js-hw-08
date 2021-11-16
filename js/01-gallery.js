09584b4fb62a6c9d8e01913b669e5c44f8ef824c
Oleksii Ostafiichuk <issimo333@gmail.com>
2021-11-16 22:18:49 +0200

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryImages = galleryItems
+  .map(
+    image =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img
+                class="gallery__image"
+                src="${image.preview}"
+                alt="${image.description}"
+            />
+        </a>
+    </li>`,
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryImages);
+
+const settingsSimpleLightbox = event => {
+  event.preventDefault();
+  new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+};
+
+gallery.addEventListener('click', settingsSimpleLightbox);
